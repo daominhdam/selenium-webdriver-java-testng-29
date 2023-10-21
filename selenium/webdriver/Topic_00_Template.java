c536f9e99b4ecfac951e8b29a1f0cc14290520b4
Dao Minh Dam <daominhdam@gmail.com>
2023-10-21 08:58:26 +0700

    Update Template

--- selenium/webdriver/Topic_00_Template.java
+++ selenium/webdriver/Topic_00_Template.java
@@ -12,8 +12,6 @@ import java.util.concurrent.TimeUnit;
 
 public class Topic_00_Template {
     WebDriver driver;
-    String projectPath = System.getProperty("user.dir");
-    String osName = System.getProperty("os.name");
 
     @BeforeClass
     public void beforeClass() {
@@ -24,23 +22,6 @@ public class Topic_00_Template {
     @Test
     public void TC_01_() {
 
-        driver.get("http://live.techpanda.org/index.php/customer/account/login/");
-
-        // Tab Accesibility/ Properties trong Elements
-        String accessibleName = driver.findElement(By.cssSelector("input#email")).getAccessibleName();
-        System.out.println("Accessible name = " + accessibleName);
-
-        driver.get("https://bubble.io/login?mode=signup");
-
-        // https://www.codeinwp.com/blog/wai-aria-roles/
-        String ariaRole = driver.findElement(By.cssSelector("iframe[name*='__privateStripeMetricsController']")).getAriaRole();
-        System.out.println("Aria Role = " + ariaRole);
-
-//        driver.findElement(By.id("")).getDomAttribute("checked");
-//        driver.findElement(By.id("")).getDomProperty("baseURI");
-//        driver.findElement(By.id("")).getDomProperty("outerHTML");
-//
-//        driver.findElement(By.id("")).getShadowRoot();
     }
 
     @Test
