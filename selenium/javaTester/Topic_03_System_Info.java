25d1ecfa6f87cda25e50339466b0fa82ddadc629
Dao Minh Dam <daominhdam@gmail.com>
2023-11-21 22:01:53 +0700

    Popup Part II

--- selenium/javaTester/Topic_03_System_Info.java
+++ selenium/javaTester/Topic_03_System_Info.java
@@ -6,6 +6,10 @@ import org.openqa.selenium.Platform;
 public class Topic_03_System_Info {
     public static void main(String args[]) {
 
+        // Lấy ra đường dẫn tương đối tại thư mục hiện tại
+        String projectPath = System.getProperty("user.dir");
+        System.out.println(projectPath + "\\browserDrivers\\geckodriver.exe");
+
         String osName = System.getProperty("os.name");
         Keys keys;
 
--- selenium/webdriver/Topic_03_Relative_Locator.java
+++ selenium/webdriver/Topic_03_Relative_Locator.java
@@ -9,24 +9,17 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Topic_03_Relative_Locator {
     WebDriver driver;
-    String projectPath = System.getProperty("user.dir");
-    String osName = System.getProperty("os.name");
 
     @BeforeClass
     public void beforeClass() {
-        if (osName.contains("Windows")) {
-            System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
-        } else {
-            System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
-        }
-
         driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         driver.manage().window().maximize();
     }
 
