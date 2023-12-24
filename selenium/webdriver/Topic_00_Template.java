86254387b7ff610b7c61a5d13a7ed33c2a5747be
Dao Minh Dam <daominhdam@gmail.com>
2023-12-24 17:52:56 +0700

    Wait P3

--- selenium/webdriver/Topic_00_Template.java
+++ selenium/webdriver/Topic_00_Template.java
@@ -2,13 +2,14 @@ package webdriver;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
+import java.util.List;
 
 public class Topic_00_Template {
     WebDriver driver;
@@ -17,6 +18,7 @@ public class Topic_00_Template {
     public void beforeClass() {
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
     }
 
     @Test
